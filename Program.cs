35bd45202c6c6250aec336e136feaf734ef903da
j-suncheon <whaql78@gamil.com>
2022-09-19 14:39:34 +0900

    9.2_C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using System.Collections;
+
+namespace H
+{
+    class Program
+    {
+        static void Main(string[] argc)
+        {
+           
+        }
+    }
+
+    class Fraction
+    {
+        public int numerator;
+        public int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+              
+        public static int CommonMeasure(int a, int b)
+        {
+            
+        }
+    }
+}
